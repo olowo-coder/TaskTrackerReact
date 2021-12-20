5591e06b3faedec97dbd0e07a470e65b4f286796
olowo-coder <olowofela.olanrewaju@decagon.dev>
2021-12-21 00:09:47 +0100

    final commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import Button from './Button';
+import { useLocation } from  'react-router-dom';
 
 function Header({ title, onAdd, showAdd }) {
     const onClick = () =>{
         console.log('clicked')
     }
 
+    const location = useLocation();
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button 
+            {location.pathname === '/' && <Button 
             onClick={onAdd}
             color={showAdd ? 'red' : 'green'} 
-            text={showAdd ? 'Close' : 'Add'} />
+            text={showAdd ? 'Close' : 'Add'} />}
         </header>
     )
 }
