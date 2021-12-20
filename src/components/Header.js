1a0a08d9b3e658c9046493e579f392e4bd6ea1ae
olowo-coder <olowofela.olanrewaju@decagon.dev>
2021-12-20 17:18:08 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+function Header({ title }) {
+    const onClick = () =>{
+        console.log('clicked')
+    }
+
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button 
+            onClick={onClick}
+            color='green' 
+            text='Add' />
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: ' Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+    color: 'red', backgroundColor: 'gray'
+}
+
+export default Header
